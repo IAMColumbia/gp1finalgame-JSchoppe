ce5038e694ebbb877dcfcf485839929c29886fcc
JSchoppe <jethro.schoppenhorst@gmail.com>
2020-11-27 14:25:30 -0600

    adds test scene for player cursor
    adds a test scene and corresponding prefabs for the player cursor

---
+++ Skirmish Wars/Assets/Scenes/Test Scenes/PlayerCursorTest.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public sealed class PlayerCursorTest : MonoBehaviour
+{
+    [SerializeField] private TileGrid grid = null;
+
+    private void Start()
+    {
+        grid.ParseSceneUnitsOntoGrid();
+    }
+}
