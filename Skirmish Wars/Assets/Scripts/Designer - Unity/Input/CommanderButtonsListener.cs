907351607432dc056974d6bb7a401a0da2cd3b59
JSchoppe <jethro.schoppenhorst@gmail.com>
2020-12-14 19:09:27 -0600

    implements pausing and game exiting
    using the new unity input system binds pause and exit functionality for the game

---
+++ Skirmish Wars/Assets/Scripts/Designer - Unity/Input/CommanderButtonsListener.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace SkirmishWars.Unity
+{
+    /// <summary>
+    /// Buttons listener implemented using the new Unity Input System.
+    /// </summary>
+    public sealed class CommanderButtonsListener : MonoBehaviour
+    {
+        #region Exposed Events
+        /// <summary>
+        /// Called when this buttons listener registers a pause button press.
+        /// </summary>
+        public event Action PausePressed;
+        /// <summary>
+        /// Called when this buttons listener registers an exit button press.
+        /// </summary>
+        public event Action ExitPressed;
+        #endregion
+        #region New Input Implementation
+        public void OnPauseButtonAction(InputAction.CallbackContext context)
+        {
+            if (context.ReadValueAsButton() && !context.performed)
+                PausePressed?.Invoke();
+        }
+        public void OnExitButtonAction(InputAction.CallbackContext context)
+        {
+            if (context.ReadValueAsButton() && !context.performed)
+                ExitPressed?.Invoke();
+        }
+        #endregion
+    }
+}
